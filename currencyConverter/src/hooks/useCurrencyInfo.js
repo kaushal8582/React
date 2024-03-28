c968303f9dedd331e7a77e5c0280dcba162fccd9
kaushal <kaushalkumar8582@gmail.com>
2024-03-28 16:39:56 +0530

    update

---
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,23 @@
+import { useState,useEffect } from "react";
+
+
+
+function useCurrencyInfo(currency){
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+        const [data,setData] = useState({});
+        useEffect(()=>{
+        fetch(url)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+        .catch((err)=>console.log(err))
+        // console.log(data);
+    },[currency])
+
+    // console.log(data);
+    return data;
+
+}
+
+export default useCurrencyInfo;
+
+
